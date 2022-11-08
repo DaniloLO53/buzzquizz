9db67e2b5f746373cd923fa738bb0eb0d6eab331
Danilo Leão <olin_ad@hotmail.com>
2022-11-08 15:55:36 -0300

    added initial quiz info form and validation

--- myVersion/scripts/script.js
+++ myVersion/scripts/script.js
@@ -1,6 +1,37 @@
 const main = document.querySelector('main');
 const allQuizzesContainer = document.querySelector('.allQuizzesContainer');
 const myQuizzes = document.querySelector('.myQuizzes');
+let initialQuizzValid = false;
+
+const quiz = {
+  initial: [
+    {
+      placeholder: 'Título do seu quizz',
+      name: 'title',
+      type: 'text',
+      minlength: 20,
+      maxlength: 65
+    },
+    {
+      placeholder: 'URL da imagem do seu quizz',
+      name: 'url',
+      type: 'text',
+
+    },
+    {
+      placeholder: 'Quantidade de perguntas do quizz',
+      name: 'questionsNumber',
+      type: 'number',
+      min: 3
+    },
+    {
+      placeholder: 'Quantidade de níveis do quizz',
+      name: 'levelsNumber',
+      type: 'number',
+      min: 2
+    }
+  ],
+}
 
 function reloadToInitialScreen() {
   const container = document.querySelector('.errorContainer');
@@ -108,6 +139,122 @@ async function buildQuizzes() {
   console.log(quizzes);
 };
 
+function renderQuestionsCreator(event) {
+  const [form, button] = renderGeneralQuiz(event);
+  formBuilder(form, 'questions');
+  // button.addEventListener('click', renderQuestionsCreator);
+};
+
+function renderQuizCreator(event) {
+  const [form, button] = renderGeneralQuiz(event);
+  formBuilder(form, 'initial');
+  button.addEventListener('click', renderQuestionsCreator);
+};
+
+function renderGeneralQuiz(event) {
+  const { target } = event;
+  const text = target.innerText;
+
+  Array.from(main.children).map((child) => child.remove());
+
+  const startQuizzContainer = document.createElement('article');
+  const title = document.createElement('h3');
+  const form = document.createElement('form');
+  const button = document.createElement('button');
+
+  button.disabled = true;
+  title.innerHTML = text === 'Criar Quizz'
+    ? 'Comece pelo começo'
+    : 'Prosseguir pra criar perguntas'
+      ? 'Crie suas perguntas'
+      : 'Agora, decidaq os níveis!';
+  button.innerHTML = text === 'Criar Quizz'
+    ? 'Prosseguir pra criar perguntas'
+    : 'Prosseguir pra criar perguntas'
+      ? 'Prosseguir pra criar níveis'
+      : 'Finalizar Quizz';
+
+  startQuizzContainer.classList.add('startQuizzContainer');
+  button.classList.add('startQuizzContainerButton');
+
+  main.append(startQuizzContainer);
+  startQuizzContainer.append(title);
+  startQuizzContainer.append(form);
+  startQuizzContainer.append(button);
+
+  return [form, button];
+};
+
+function formBuilder(parent, type) {
+  if (type === 'initial') {
+    for (let index = 0; index < 4; index += 1) {
+      const label = document.createElement('label');
+      const input = document.createElement('input');
+
+      input.addEventListener('change', validateInitialQuizzInfo)
+
+      input.placeholder = quiz.initial[index].placeholder;
+      input.name = quiz.initial[index].name;
+      input.type = quiz.initial[index].type;
+      if (input.type === 'number') input.min = quiz.initial[index].min;
+      if (input.name === 'title') {
+        input.setAttribute('minlength', quiz.initial[index].minlength);
+        input.setAttribute('maxlength', quiz.initial[index].maxlength);
+      };
+
+      label.append(input);
+      parent.append(label);
+    }
+  } else if (type === 'questions') {
+
+  }
+};
+
+function validateInitialQuizzInfo(event) {
+  const { name, value } = event.target;
+  const { initial } = quiz;
+  const button = document.querySelector('.startQuizzContainerButton');
+  const input = initial.find((input) => input.name === name);
+  input.value = value;
+
+  const invalidTitle = document.createElement('p');
+  const invalidUrl = document.createElement('p');
+  invalidTitle.innerHTML = 'Mínimo: 20 caracteres, Máximo: 65 caracteres';
+  invalidUrl.innerHTML = 'Insira uma URL válida';
+
+  invalidTitle.classList.add('invalidText');
+  invalidTitle.classList.add('invalidTitle');
+  invalidUrl.classList.add('invalidText');
+  invalidUrl.classList.add('invalidUrl');
+
+  const titleValid = initial[0].value?.length >= 20 && initial[0]?.value?.length <= 65;
+  if (!titleValid && event.target.name === 'title') {
+    console.log(document.querySelector('.invalidText'));
+    document.querySelector('.invalidTitle')?.remove();
+    event.target.after(invalidTitle);
+  } else if (event.target.name === 'title') {
+    document.querySelector('.invalidTitle')?.remove();
+  }
+
+  const urlValid = /((?:(?:http?|ftp)[s]*:\/\/)?[a-z0-9-%\/\&=?\.]+\.[a-z]{2,4}\/?([^\s<>\#%"\,\{\}\\|\\\^\[\]`]+)?)/gi.test(initial[1].value);
+  if (!urlValid && event.target.name === 'url') {
+    console.log(document.querySelector('.invalidText'));
+    document.querySelector('.invalidUrl')?.remove();
+    event.target.after(invalidUrl);
+  } else if (event.target.name === 'url') {
+    document.querySelector('.invalidUrl')?.remove();
+  }
+  const questionsValid = initial[2].value >= 3;
+  const levelsValid = initial[3].value >= 2;
+
+  initialQuizzValid = titleValid && urlValid && questionsValid && levelsValid;
+  if (initialQuizzValid) {
+    button.removeAttribute('disabled');
+  } else {
+    button.setAttribute('disabled', true);
+  }
+};
+
 function buildNoQuizzes() {
   const noQuizzesContainer = document.createElement('div');
   const noQuizzesText = document.createElement('p');
@@ -117,6 +264,8 @@ function buildNoQuizzes() {
 
   noQuizzesText.innerHTML = 'Você não criou nenhum quizz ainda :('
   createQuizzButton.innerHTML = 'Criar Quizz';
+
+  createQuizzButton.addEventListener('click', renderQuizCreator);
 
   myQuizzes.append(noQuizzesContainer);
   noQuizzesContainer.append(noQuizzesText);
